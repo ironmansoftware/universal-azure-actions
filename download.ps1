1ffb33b3975b8c4abc99fbff839ea19e14f58fda
Adam Driscoll <adam.r.driscoll@gmail.com>
2020-09-28 11:12:03 -0600

    Copy to output

--- download.ps1
+++ download.ps1
@@ -1,3 +1,5 @@
 $Version = (Invoke-WebRequest https://imsreleases.blob.core.windows.net/universal/production/version.txt).Content
 Invoke-WebRequest "https://imsreleases.blob.core.windows.net/universal/production/$Version/Universal.win-x64.$Version.zip" -OutFile ".\pu.zip"
 Expand-Archive -Path .\pu.zip -DestinationPath .\pu
+Copy-Item .\appsettings.json .\pu -Force 
+Copy-Item .\web.config .\pu -Force 
