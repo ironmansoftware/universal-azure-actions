2bad3302bf2107abe6101ac7b26318767ca7ce42
Adam Driscoll <adamdriscoll@users.noreply.github.com>
2020-11-17 10:28:17 -0700

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,5 +1,5 @@
 $Version = (Invoke-WebRequest https://imsreleases.blob.core.windows.net/universal/production/version.txt).Content
-Invoke-WebRequest "https://imsreleases.blob.core.windows.net/universal/production/$Version/Universal.win-x64.$Version.zip" -OutFile ".\pu.zip"
+Invoke-WebRequest "https://imsreleases.blob.core.windows.net/universal/production/$Version/Universal.win7-x64.$Version.zip" -OutFile ".\pu.zip"
 Expand-Archive -Path .\pu.zip -DestinationPath .\pu
 Copy-Item .\appsettings.json .\pu -Force 
 Copy-Item .\web.config .\pu -Force 
